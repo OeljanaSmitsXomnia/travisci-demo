81a7cc50c841e5d1b8b5901432234e35f11e364e
Smits <oeljana.smits@frieslandcampina.com>
2020-05-22 12:40:33 +0200

    code coverage added

--- tests/test_flaskapp.py
+++ tests/test_flaskapp.py
@@ -11,36 +11,14 @@ class MyTestCase(unittest.TestCase):
 
     def test_multiply(self):
         #given
-        x = "10"
-        y = "12"
+        x = 10
+        y = 12
 
         #when
         answer = multiply(x,y)
 
         #then
-        with self.assertRaises(typeError):
-            answer = multiply(x,y)
-
-    def test_to_upper(self):
-        # given
-        s = 'hi'
-
-        # when
-        answer = to_uppercase(s)
-
-        # then
-        self.assertEqual(answer, 'HI')
-
-    def test_hello(self):
-        # given
-
-        # when
-        answer = hello()
-
-        # then
-        self.assertEqual("Hello World!")
-
-
-
+         self.assertEqual(answer, 120)
+   
 if __name__ == '__main__':
     unittest.main()
